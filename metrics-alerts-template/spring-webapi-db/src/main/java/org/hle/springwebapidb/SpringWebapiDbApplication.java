75f688613ff8d720933c38f1675d55d853b06c7f
yinghanhung <hle.infdy@gmail.com>
2026-01-01 00:10:37 +0800

    init spring metric demo project

---
+++ metrics-alerts-template/spring-webapi-db/src/main/java/org/hle/springwebapidb/SpringWebapiDbApplication.java
@@ -0,0 +1,13 @@
+package org.hle.springwebapidb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebapiDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebapiDbApplication.class, args);
+    }
+
+}
---
+++ metrics-alerts-template/spring-webapi-db/src/test/java/org/hle/springwebapidb/SpringWebapiDbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hle.springwebapidb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebapiDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
